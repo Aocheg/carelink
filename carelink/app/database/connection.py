90322eec516e9cbf9955c0e4b6a9122afb06283c
Aocheg <oche8004@gmail.com>
2026-09-07 10:52:47 +0100

    carelink_project_unit_1_database

--- carelink/app/database/connection.py
+++ carelink/app/database/connection.py
@@ -1,10 +1,27 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 DATABASE_URL = "sqlite:///./carelink.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connet_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
 
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
